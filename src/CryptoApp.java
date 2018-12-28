c60784261724dae57676e1aa842a8d03b4abfe4b
Odysseas <ofilippidis@citycollege.sheffield.eu>
2018-12-28 21:13:23 +0200

    Software completed.

--- src/CryptoApp.java
+++ src/CryptoApp.java
@@ -1,22 +1,7 @@
-import javax.crypto.NoSuchPaddingException;
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
-
 public class CryptoApp
 {
     public static void main(String[] args)
     {
-//        try {
-//            FileCrypto fileCrypto = new FileCrypto(PASSWORD);
-//            CryptoGui gui = new CryptoGui(fileCrypto);
-//            gui.setVisible(true);
-//        } catch (NoSuchAlgorithmException e) {
-//            e.printStackTrace();
-//        } catch (NoSuchPaddingException e) {
-//            e.printStackTrace();
-//        } catch (InvalidKeySpecException e) {
-//            e.printStackTrace();
-//        }
         LoginGui loginGui = new LoginGui();
         loginGui.setVisible(true);
     }
